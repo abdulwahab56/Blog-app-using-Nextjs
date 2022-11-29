3a0080467fcabc4047e7be42b32ad611f368db49
abdulwahab56 <108251096+abdulwahab56@users.noreply.github.com>
2022-11-29 11:02:54 +0500

    some changing in file

--- components/section2.js
+++ components/section2.js
@@ -1,73 +1,57 @@
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 import Author from "./_child/author";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Autoplay } from "swiper";
-
-// Import Swiper styles
-import "swiper/css";
-
-const Section1 = () => {
-  SwiperCore.use([Autoplay]);
+const Section2 = () => {
   return (
-    <section className="py-16 ">
+    <section className="py-16">
       <div className="container max-auto md:px-20">
         <h1 className="text-3xl font-bold uppercase pb-12 text-center">
-          Trending
+          Latest posts
         </h1>
-
-        <Swiper
-          slidesPerView={1}
-          loop={true}
-          autoplay={{
-            delay: 2000,
-          }}
-        >
-          <SwiperSlide> {Slider()}</SwiperSlide>
-          <SwiperSlide> {Slider()}</SwiperSlide>
-          <SwiperSlide> {Slider()}</SwiperSlide>
-          <SwiperSlide> {Slider()}</SwiperSlide>
-        </Swiper>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-14 ">
+          {posts()}
+          {posts()}
+          {posts()}
+        </div>
       </div>
     </section>
   );
 };
 
-export default Section1;
+export default Section2;
 
-function Slider() {
+function posts() {
   return (
-    <div className="grid md:grid-cols-2 ">
-      <div className="image">
+    <div>
+      <div className="post-imag">
         <Link href={"/"}>
           <Image
-            src={"/images/blog1.jpg"}
+            src={"/images/post1.jpg"}
             alt="blog iamge"
             width={500}
-            height={450}
+            height={300}
             className="shadow-lg rounded-lg"
           />
         </Link>
       </div>
-      <div className="type">
+      <div className="post-detail flex justify-center flex-col py-4">
         <div className="flex">
           <Link href={"/"}>
-            <p className="text-2xl text-orange-800 hover:text-orange-600">
+            <p className="text-xl text-orange-800 hover:text-orange-600">
               Business,Travel-
             </p>
           </Link>
-          <p className="text-2xl text-gray-800 hover:text-gray-600">
+          <p className="text-xl text-gray-800 hover:text-gray-600">
             July,3,2022
           </p>
         </div>
         <div className="title">
           <Link href={"/"}>
-            <span className="text-3xl md:text-6xl font-bold text-gray-800 hover:text-gray-600">
+            <span className="text-xl font-bold text-gray-800 hover:text-gray-600">
               Your most unhappy customers are your greate source of learning
             </span>
           </Link>
-          <p className="py-3">
+          <p className="py-3 text-sm">
             Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
             commodo ligula eget dolor. Aenean massa. Cum sociis natoque
             penatibus et magnis dis parturient montes, nascetur ridiculus mus.
--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,13 @@ import Image from "next/image";
 import Format from "../layout/format";
 // components import
 import Section1 from "../components/section1";
+import Section2 from "../components/section2";
 
 export default function Home() {
   return (
     <Format>
       <Section1 />
+      <Section2 />
     </Format>
   );
 }
